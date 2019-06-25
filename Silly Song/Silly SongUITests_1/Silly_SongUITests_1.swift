9e9cbbdae79fda5c6d22bfb6be4de91fab231fff
Mayur yewale <mayur.yewale@mindbodyonline.com>
2019-06-25 16:52:08 +0530

    Added Comment and validation

--- Silly Song/Silly SongUITests_1/Silly_SongUITests_1.swift
+++ Silly Song/Silly SongUITests_1/Silly_SongUITests_1.swift
@@ -18,13 +18,13 @@ class Silly_SongUITests_1: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
+    //Test case to validate label on app
     func test_validate_label(){
         let label = XCUIApplication().staticTexts["Generate a Silly Song"]
-        XCTAssertNotNil(label.label.range(of:"Generate a Silly Song"))
+    XCTAssertNotNil(label.label.range(of:"Generate a Silly Song"))
         XCTAssertTrue(label.exists)
     }
-    
+    //Test case to validate placeholder in textfield
     func test_validate_placeholder(){
         let app = XCUIApplication()
         let enterYourNameTextField = app.textFields["Enter your name"]
@@ -32,19 +32,19 @@ class Silly_SongUITests_1: XCTestCase {
         var str = enterYourNameTextField.placeholderValue
         XCTAssertEqual("Enter your name", str)
     }
-    
+    //Test case to validate textfield
     func test_validate_textfield(){
         let app = XCUIApplication()
         let enterYourNameTextField = app.textFields["Enter your name"]
         XCTAssertTrue(enterYourNameTextField.exists)
     }
-    
+    //Test case to validate textview
     func test_validate_textview(){
         let app = XCUIApplication()
         let textview=app.otherElements.containing(.staticText, identifier:"Generate a Silly Song").children(matching: .textView).element
         XCTAssertTrue(textview.exists)
     }
-    
+    //Test case to validate textview output
     func test_validate_textviewoutput() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -67,7 +67,7 @@ class Silly_SongUITests_1: XCTestCase {
         enterYourNameTextField.tap()
         XCUIApplication().terminate()
     }
-    
+    //Test case to enter text which start with vowel and end with consonant
     func test_text_startwith_vowel_and_then_consonant() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -75,17 +75,22 @@ class Silly_SongUITests_1: XCTestCase {
         let enterYourNameTextField = app.textFields["Enter your name"]
         
         enterYourNameTextField.tap()
-        enterYourNameTextField.typeText("Eed")
+        enterYourNameTextField.typeText("EED")
         let generateASillySongElement = app.otherElements.containing(.staticText, identifier:"Generate a Silly Song").element
         
         generateASillySongElement.tap()
         generateASillySongElement.tap()
         
         let textview=app.otherElements.containing(.staticText, identifier:"Generate a Silly Song").children(matching: .textView).element
+        
+        let x=textview.value  as! String
+        let str="Eed, Eed, Bo Beed\nBanana Fana Fo Feed\nMe My Mo Meed\nEed"
+        
+        XCTAssertEqual(x, str)
         enterYourNameTextField.tap()
         XCUIApplication().terminate()
     }
-    
+    //Test case to enter text which start with consonant and end with vowel
     func test_text_startwith_Consonant_and_then_vowel() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -93,17 +98,21 @@ class Silly_SongUITests_1: XCTestCase {
         let enterYourNameTextField = app.textFields["Enter your name"]
         
         enterYourNameTextField.tap()
-        enterYourNameTextField.typeText("Ted")
+        enterYourNameTextField.typeText("TED")
         let generateASillySongElement = app.otherElements.containing(.staticText, identifier:"Generate a Silly Song").element
         
         generateASillySongElement.tap()
         generateASillySongElement.tap()
         
         let textview=app.otherElements.containing(.staticText, identifier:"Generate a Silly Song").children(matching: .textView).element
+        
+        let x=textview.value  as! String
+        let str="Ted, Ted, Bo Bed\nBanana Fana Fo Fed\nMe My Mo Med\nTed"
+        XCTAssertEqual(x, str)
         enterYourNameTextField.tap()
         XCUIApplication().terminate()
     }
-    
+    //Test case to enter text which has all consonant
     func test_allConsonants() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -118,10 +127,15 @@ class Silly_SongUITests_1: XCTestCase {
         generateASillySongElement.tap()
         
         let textview=app.otherElements.containing(.staticText, identifier:"Generate a Silly Song").children(matching: .textView).element
+        
+        let x=textview.value  as! String
+        let str="Mmmm, Mmmm, Bo Bmmmm\nBanana Fana Fo Fmmmm\nMe My Mo Mmmmm\nMmmm"
+        
+        XCTAssertEqual(x, str)
         enterYourNameTextField.tap()
         XCUIApplication().terminate()
     }
-    
+    //Test case to enter text which has all vowels
     func test_allVowels() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -136,6 +150,10 @@ class Silly_SongUITests_1: XCTestCase {
         generateASillySongElement.tap()
         
         let textview=app.otherElements.containing(.staticText, identifier:"Generate a Silly Song").children(matching: .textView).element
+        let x=textview.value  as! String
+        let str="Aaaa, Aaaa, Bo Baaaa\nBanana Fana Fo Faaaa\nMe My Mo Maaaa\nAaaa"
+        
+        XCTAssertEqual(x, str)
         XCTAssertTrue(textview.exists)
         enterYourNameTextField.tap()
         XCUIApplication().terminate()
